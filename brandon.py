f1f7c55e77be53bdcf01e5cbb857cf86adc43b37
AznSensationXIII <bse36@cornell.edu>
2013-10-09 16:26:59 -0400

    Added the python script for my (brandon) info.
    My script prints out basic student information about myself
    including name, year, Baja subteam, and hometown.

---
+++ brandon.py
@@ -0,0 +1,7 @@
+# Really Simple Python Script
+# Prints out Student Information
+print "Cornell Baja SAE!"
+print "Name:     Brandon Eusebio"
+print "Year:     2015"
+print "Subteam:  ECE"
+print "Hometown: Las Vegas, NV (yeah baby)"
