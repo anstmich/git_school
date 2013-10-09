1716e38507076147a2b65594ec7362c1a2d7fca6
AznSensationXIII <bse36@cornell.edu>
2013-10-09 16:48:29 -0400

    Changed the fox to ask for students' interpretations.
    Students will need to fill in what the fox says.

--- fox.py
+++ fox.py
@@ -2,31 +2,15 @@
 #
 # For those who have been living under a rock:
 # http://www.youtube.com/watch?v=jofNR_WkoCE
-import random as rand
-
-FOX_PHRASES = [ "Ring-ding-ding-ding-dingeringeding!",
-			    "Gering-ding-ding-ding-dingeringeding!",
-			    "Wa-pa-pa-pa-pa-pa-pow!",
-			    "Hatee-hatee-hatee-ho!",
-			    "Joff-tchoff-tchoffo-tchoffo-tchoff!",
-			    "Tchoff-tchoff-tchoffo-tchoffo-tchoff!",
-			    "Jacha-chacha-chacha-chow!",
-			    "Chacha-chacha-chacha-chow!",
-			    "Fraka-kaka-kaka-kaka-kow!",
-			    "A-hee-ahee ha-hee!",
-			    "A-oo-oo-oo-ooo!", 
-			    "Woo-oo-oo-ooo!" ]
 
 def whatDoesTheFoxSay():
-	return FOX_PHRASES[int(rand.random()*(len(FOX_PHRASES)-1))]
+	#TODO: Change me
+	foxSays = "???"
+	return foxSays
 
 def main():
-	f = open("song.txt", 'r')
-	for line in f:
-		if line.rstrip('\n') == "FOX":
-			print whatDoesTheFoxSay()
-		else:
-			print line.rstrip('\n')
+	print "What does the fox say?"
+	print whatDoesTheFoxSay()
 
 if __name__ == "__main__":
 	main()
